3b1bfe06d458616e050169adfed3fcfb497eb7fc
Maciej Ptak <maciekptk@gmail.com>
2018-09-08 21:51:10 +0200

    Zajecia6 Grid na localhost lub ubuntu

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -2,8 +2,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.MainPage;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest {
 
@@ -15,10 +20,19 @@ public class BaseTest {
 
     @Before
     public void initializeDriverAndMaximize() {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-        driver = new ChromeDriver();
+//        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+//        options.setCapability("browserName", "chrome");
+//        options.setCapability("platform", "WIN10");
+        options.addArguments("--start-maximized");
+        try {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
+//            driver = new RemoteWebDriver(new URL("http://192.168.1.105:4444/wd/hub"), options);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         wait = new WebDriverWait(driver, 5);
-        driver.manage().window().maximize();
+//        driver.manage().window().maximize();
         driver.navigate().to(url);
         mainPage = new MainPage(driver, wait);
     }
